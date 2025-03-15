8dd6f309f0f71c86f88f902cabb9f440cbc02c3d
FransiscusSuhargo <s160422070@student.ubaya.ac.id>
2025-03-15 22:53:55 +0700

    Ubah migration user

--- database/migrations/2025_03_15_153610_create_users_table.php
+++ database/migrations/2025_03_15_153610_create_users_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->integer('id')->primary();
-            $table->string('email', 45)->nullable();
-            $table->string('password', 45)->nullable();
-            $table->string('personal_access_token', 45)->nullable();
-            $table->string('email_verified', 45)->nullable();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
         });
     }
 
